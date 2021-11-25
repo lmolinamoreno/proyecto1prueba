6ce63b39d33917195984ae7f6a1a079e107508fb
lmolmor915@g.educaand.es <lmolmor915@g.educaand.es>
2021-11-25 23:23:41 +0100

    Version 0.0.2

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-xoáuefpgoqjaasipfhasduoijg
-no se javascript
-usroñserilñ
+var expect = require('chai').expect;
+
+require('./app.js');
+
+
+    describe('INFINITY', function()
+    {
+        it('INFINITY === "INFINITY"',
+            function()
+            {
+                expect(INFINITY)
+                    .to.equal('INFINITY');
+            });
+    });
